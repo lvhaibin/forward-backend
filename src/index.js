0ac5a0c9c512c4f6ecfc1b03e54c22ac6c982a6f
lvhaibin <lvhaibin@chengfayun.com>
2020-09-16 22:15:32 +0800

    feat: token

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import KoaRouter from 'koa-router';
 import koaBody from 'koa-body';
 import KoaJwt from 'koa-jwt';
 import { appRoutes } from './routers/index.js';
+import jwt from 'jsonwebtoken';
 
 const app = new Koa();
 const router = new KoaRouter()
 
 appRoutes.forEach(route => router[route.method](route.path, route.action));
 
-app.use(koaBody()).use(router.routes())
+app.use(koaBody())
     .use(KoaJwt({
         secret: 'forward_secret',
     }).unless({
-        path: [/^api\/v1.0\/login/]
+        path: [/api\/v1.0\/login/]
     })).use((ctx, next) => {
         if (ctx.header && ctx.header.authorization) {
             const parts = ctx.header.authorization.split(' ');
@@ -24,21 +25,27 @@ app.use(koaBody()).use(router.routes())
                 if (/^Bearer$/i.test(scheme)) {
                      try {
                         //jwt.verify方法验证token是否有效
-                        jwt.verify(token, secret.sign, { complete: true }); 
+                         jwt.verify(token, 'forward_secret', { complete: true }); 
                     } catch (error) {
-                        ctx.status = 102;
-                        ctx.body = 'token 已过期！'; 
+                        ctx.body = {
+                            code: 102,
+                            msg: 'token 已过期！',
+                            body: null
+                        }
                     } 
                 } 
             } 
         }
         return next().catch(err => {
             if (err.status === 102) {
-                ctx.status = 102;
-                ctx.body = 'token 已过期！'; 
+                ctx.body = {
+                    code: 102,
+                    msg: 'token 已过期！',
+                    body: null
+                }
             } else {
                 throw err;
             }
         });
-    });
+    }).use(router.routes());
 app.listen(3000);
