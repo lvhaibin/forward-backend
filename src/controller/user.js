6e54d828bc7d926b6c5f3614d2dbab0a1948fad3
lvhaibin <646803578@qq.com>
2020-09-16 13:46:54 +0800

    feat: token

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,5 +1,7 @@
 import query from '../connection/mysql.js';
-import crypto from 'crypto'
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+
 function md5(data) {
     // 以md5的格式创建一个哈希值
     let hash = crypto.createHash('md5');
@@ -21,18 +23,28 @@ class User {
 
     async login(ctx) {
         const { username, password } = ctx.request.body;
-        const res = await query(`select user_name, phone, password from user where user_name='${username}';`);
-        if (res[0].password == md5(password)) {
+        const res = await query(`select id, user_name, phone, password, email from user where user_name='${username}';`);
+        if (!res) {
+            ctx.body = {
+                code: 101,
+                msg: '用户不存在！',
+                body: null
+            }
+        } else if (res[0].password != md5(password)) {
+            ctx.body = {
+                code: 101,
+                msg: '密码错误',
+                body: null
+            }
+        } else if (res[0].password === md5(password)) {
+            const payload = { userName: res[0].user_name, id: res[0].id, email: res[0].email };
+            const token = jwt.sign(payload, 'forward_secret', { expiresIn: '12h' });
             ctx.body = {
                 code: 0,
                 msg: 'success',
-                body: null
-            }
-        } else {
-            ctx.body = {
-                code: 0,
-                msg: 'faild',
-                body: null
+                body: {
+                    token
+                }
             }
         }
     }
--- src/index.js
+++ src/index.js
@@ -12,5 +12,33 @@ appRoutes.forEach(route => router[route.method](route.path, route.action));
 app.use(koaBody()).use(router.routes())
     .use(KoaJwt({
         secret: 'forward_secret',
-    }));
+    }).unless({
+        path: [/^api\/v1.0\/login/]
+    })).use((ctx, next) => {
+        if (ctx.header && ctx.header.authorization) {
+            const parts = ctx.header.authorization.split(' ');
+            if (parts.length === 2) {
+                 //取出token
+                const scheme = parts[0];
+                const token = parts[1];
+                if (/^Bearer$/i.test(scheme)) {
+                     try {
+                        //jwt.verify方法验证token是否有效
+                        jwt.verify(token, secret.sign, { complete: true }); 
+                    } catch (error) {
+                        ctx.status = 102;
+                        ctx.body = 'token 已过期！'; 
+                    } 
+                } 
+            } 
+        }
+        return next().catch(err => {
+            if (err.status === 102) {
+                ctx.status = 102;
+                ctx.body = 'token 已过期！'; 
+            } else {
+                throw err;
+            }
+        });
+    });
 app.listen(3000);
